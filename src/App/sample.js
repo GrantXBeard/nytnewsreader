6ae88bf13dce0d9897e99fd6d63440b7a91ba47c
Grant Beard <Grantxbeard@gmail.com>
2022-09-13 10:54:58 -0600

    add sample data

---
+++ src/App/sample.js
@@ -0,0 +1,261 @@
+export default sampleData = {
+    "status": "OK",
+    "copyright": "Copyright (c) 2022 The New York Times Company. All Rights Reserved.",
+    "section": "The New York Times Magazine",
+    "last_updated": "2022-09-13T09:32:31-04:00",
+    "num_results": 28,
+    "results":[
+    {
+      "section": "magazine",
+      "subsection": "",
+      "title": "Current Issue",
+      "abstract": "",
+      "url": "null",
+      "uri": "nyt://embeddedinteractive/dbe009f8-1c63-596d-a2f5-22b6c87e5704",
+      "byline": "",
+      "item_type": "EmbeddedInteractive",
+      "updated_date": "2022-09-09T16:08:01-04:00",
+      "created_date": "2020-07-02T17:22:55-04:00",
+      "published_date": "2020-07-02T17:22:55-04:00",
+      "material_type_facet": "",
+      "kicker": "",
+      "des_facet": [],
+      "org_facet": [],
+      "per_facet": [],
+      "geo_facet": [],
+      "multimedia": null,
+      "short_url": ""
+    },
+    {
+      "section": "admin",
+      "subsection": "",
+      "title": "",
+      "abstract": "",
+      "url": "",
+      "uri": "nyt://embeddedinteractive/6cb185ed-65ac-50a5-ace3-5bab81934245",
+      "byline": "",
+      "item_type": "EmbeddedInteractive",
+      "updated_date": "2015-07-20T13:45:03-04:00",
+      "created_date": "2015-04-16T14:13:18-04:00",
+      "published_date": "2015-04-16T14:13:18-04:00",
+      "material_type_facet": "",
+      "kicker": "",
+      "des_facet": [],
+      "org_facet": [],
+      "per_facet": [],
+      "geo_facet": [],
+      "multimedia": null,
+      "short_url": ""
+    },
+    {
+      "section": "magazine",
+      "subsection": "",
+      "title": "",
+      "abstract": "",
+      "url": "null",
+      "uri": "nyt://embeddedinteractive/1ce169b0-62fa-54be-b2a9-709303bfe6fb",
+      "byline": "",
+      "item_type": "EmbeddedInteractive",
+      "updated_date": "2018-06-13T19:04:01-04:00",
+      "created_date": "2018-03-23T14:36:21-04:00",
+      "published_date": "2018-03-23T14:36:21-04:00",
+      "material_type_facet": "",
+      "kicker": "",
+      "des_facet": [],
+      "org_facet": [],
+      "per_facet": [],
+      "geo_facet": [],
+      "multimedia": null,
+      "short_url": ""
+    },
+    {
+      "section": "magazine",
+      "subsection": "",
+      "title": "Join Us for a Screening of ‘The Murdochs’",
+      "abstract": "Times subscribers can get a sneak peek of the documentary series on the legacy of the Murdochs, based on New York Times reporting.",
+      "url": "https://www.nytimes.com/2022/09/07/magazine/screening-murdochs.html",
+      "uri": "nyt://article/c51a32b6-bfd7-51b6-a20a-9abd91b2c445",
+      "byline": "By The New York Times",
+      "item_type": "Article",
+      "updated_date": "2022-09-07T10:06:20-04:00",
+      "created_date": "2022-09-07T09:00:02-04:00",
+      "published_date": "2022-09-07T09:00:02-04:00",
+      "material_type_facet": "",
+      "kicker": "",
+      "des_facet": [
+        "Documentary Films and Programs",
+        "News and News Media",
+        "Television",
+        "Content Type: Personal Profile",
+        "The Murdochs: Empire of Influence (TV Program)"
+      ],
+      "org_facet": [
+        "New York Times",
+        "CNN"
+      ],
+      "per_facet": [
+        "Murdoch, Rupert",
+        "Mahler, Jonathan",
+        "Rutenberg, Jim"
+      ],
+      "geo_facet": [],
+      "multimedia": [
+        {
+          "url": "https://static01.nyt.com/images/2022/09/06/admin/06CNNMurdoch-event/06CNNMurdoch-event-superJumbo.jpg",
+          "format": "Super Jumbo",
+          "height": 1080,
+          "width": 1920,
+          "type": "image",
+          "subtype": "photo",
+          "caption": "",
+          "copyright": "Courtesy of CNN"
+        },
+        {
+          "url": "https://static01.nyt.com/images/2022/09/06/admin/06CNNMurdoch-event/06CNNMurdoch-event-threeByTwoSmallAt2X.jpg",
+          "format": "threeByTwoSmallAt2X",
+          "height": 400,
+          "width": 600,
+          "type": "image",
+          "subtype": "photo",
+          "caption": "",
+          "copyright": "Courtesy of CNN"
+        },
+        {
+          "url": "https://static01.nyt.com/images/2022/09/06/admin/06CNNMurdoch-event/06CNNMurdoch-event-thumbLarge.jpg",
+          "format": "Large Thumbnail",
+          "height": 150,
+          "width": 150,
+          "type": "image",
+          "subtype": "photo",
+          "caption": "",
+          "copyright": "Courtesy of CNN"
+        }
+      ],
+      "short_url": "https://nyti.ms/3euNqbP"
+    },
+    {
+      "section": "magazine",
+      "subsection": "",
+      "title": "Spicy Shrimp-and-Greens From an Ottolenghi Head Chef",
+      "abstract": "This dish was inspired by a childhood in the Philippines and created in a London test kitchen.",
+      "url": "https://www.nytimes.com/2022/09/07/magazine/spicy-shrimp-greens-philippines-recipe.html",
+      "uri": "nyt://article/daff9079-fc56-5875-8202-18e7c508b409",
+      "byline": "By Yotam Ottolenghi",
+      "item_type": "Article",
+      "updated_date": "2022-09-11T01:10:46-04:00",
+      "created_date": "2022-09-07T05:00:35-04:00",
+      "published_date": "2022-09-07T05:00:35-04:00",
+      "material_type_facet": "",
+      "kicker": "",
+      "des_facet": [
+        "Recipes",
+        "Cooking and Cookbooks"
+      ],
+      "org_facet": [],
+      "per_facet": [],
+      "geo_facet": [
+        "Far East, South and Southeast Asia and Pacific Areas",
+        "Hong Kong",
+        "Philippines",
+        "London (England)"
+      ],
+      "multimedia": [
+        {
+          "url": "https://static01.nyt.com/images/2022/09/11/magazine/11mag-eat/11mag-eat-superJumbo.jpg",
+          "format": "Super Jumbo",
+          "height": 1541,
+          "width": 2048,
+          "type": "image",
+          "subtype": "photo",
+          "caption": "",
+          "copyright": "Chris Simpson for The New York Times. Food stylist: Maggie Ruggiero. Prop stylist: Sophia Pappas."
+        },
+        {
+          "url": "https://static01.nyt.com/images/2022/09/11/magazine/11mag-eat/11mag-eat-threeByTwoSmallAt2X.jpg",
+          "format": "threeByTwoSmallAt2X",
+          "height": 400,
+          "width": 600,
+          "type": "image",
+          "subtype": "photo",
+          "caption": "",
+          "copyright": "Chris Simpson for The New York Times. Food stylist: Maggie Ruggiero. Prop stylist: Sophia Pappas."
+        },
+        {
+          "url": "https://static01.nyt.com/images/2022/09/11/magazine/11mag-eat/11mag-eat-thumbLarge.jpg",
+          "format": "Large Thumbnail",
+          "height": 150,
+          "width": 150,
+          "type": "image",
+          "subtype": "photo",
+          "caption": "",
+          "copyright": "Chris Simpson for The New York Times. Food stylist: Maggie Ruggiero. Prop stylist: Sophia Pappas."
+        }
+      ],
+      "short_url": "https://nyti.ms/3D49hRW"
+    },
+    {
+      "section": "magazine",
+      "subsection": "",
+      "title": "The Campaign to Troll Dr. Oz for Living in New Jersey",
+      "abstract": "John Fetterman’s race for Senate in Pennsylvania has employed an unusual campaign strategy.",
+      "url": "https://www.nytimes.com/2022/09/07/magazine/dr-oz-pennsylvania-senate-race.html",
+      "uri": "nyt://article/eb68bd8b-77b1-5cb5-8c8f-b5aa136bd368",
+      "byline": "By Alex Norcia",
+      "item_type": "Article",
+      "updated_date": "2022-09-10T22:37:45-04:00",
+      "created_date": "2022-09-07T05:00:10-04:00",
+      "published_date": "2022-09-07T05:00:10-04:00",
+      "material_type_facet": "",
+      "kicker": "Screenland",
+      "des_facet": [
+        "Elections, Senate",
+        "United States Politics and Government"
+      ],
+      "org_facet": [],
+      "per_facet": [
+        "Oz, Mehmet C",
+        "Fetterman, John (1969- )",
+        "Zinke, Ryan (1961- )",
+        "Tshibaka, Kelly",
+        "Polizzi, Nicole"
+      ],
+      "geo_facet": [
+        "Pennsylvania",
+        "New Jersey"
+      ],
+      "multimedia": [
+        {
+          "url": "https://static01.nyt.com/images/2022/09/11/magazine/11mag-Screenland-1/11mag-Screenland-1-superJumbo-v2.jpg",
+          "format": "Super Jumbo",
+          "height": 1363,
+          "width": 2048,
+          "type": "image",
+          "subtype": "photo",
+          "caption": "",
+          "copyright": "Photo illustration by Alicia Tatone"
+        },
+        {
+          "url": "https://static01.nyt.com/images/2022/09/11/magazine/11mag-Screenland-1/11mag-Screenland-1-threeByTwoSmallAt2X.jpg",
+          "format": "threeByTwoSmallAt2X",
+          "height": 399,
+          "width": 600,
+          "type": "image",
+          "subtype": "photo",
+          "caption": "",
+          "copyright": "Photo illustration by Alicia Tatone"
+        },
+        {
+          "url": "https://static01.nyt.com/images/2022/09/11/magazine/11mag-Screenland-1/11mag-Screenland-1-thumbLarge.jpg",
+          "format": "Large Thumbnail",
+          "height": 150,
+          "width": 150,
+          "type": "image",
+          "subtype": "photo",
+          "caption": "",
+          "copyright": "Photo illustration by Alicia Tatone"
+        }
+      ],
+      "short_url": "https://nyti.ms/3wYp7td"
+    },
+    ]
+}
