8d3c362827acc4d7128b74184ed47f3a25056358
Grant Beard <Grantxbeard@gmail.com>
2022-09-13 11:46:49 -0600

    update sample data

--- src/App/App.js
+++ src/App/App.js
@@ -2,8 +2,10 @@ import React from 'react'
 import './App.css';
 import Header from '../Header/Header'
 import Display from '../Display/Display';
+import sampleData from '../sample'
 
 function App() {
+  console.log(sampleData.results)
   return (
     <main>
       <Header />
