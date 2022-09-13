457507e428dc1781f737b9e988c3bf04ba35d45b
Grant Beard <Grantxbeard@gmail.com>
2022-09-13 11:31:13 -0600

    build display component

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css';
 import Header from '../Header/Header'
+import Display from '../Display/Display';
 
 function App() {
   return (
     <main>
       <Header />
+      <Display />
     </main>
     
   );
---
+++ src/Display/Display.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './Display.css'
+
+function Display(){
+    return(
+        <h2>Howdy!</h2>
+    )
+}
+
+export default Display;
