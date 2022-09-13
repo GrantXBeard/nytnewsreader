dbb3c4c540d391b5a5fbe3d889125a0d200d361d
Grant Beard <Grantxbeard@gmail.com>
2022-09-13 11:46:37 -0600

    build card component

---
+++ src/Card/Card.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './Card.css';
+
+function Card() {
+    return(
+        <h3>Hallo</h3>
+    )   
+}
+
+export default Card;
--- src/Display/Display.js
+++ src/Display/Display.js
@@ -1,9 +1,13 @@
 import React from "react";
+import Card from "../Card/Card";
 import './Display.css'
 
 function Display(){
     return(
-        <h2>Howdy!</h2>
+        <>
+            <h2>Howdy!</h2>
+            <Card />
+        </>
     )
 }
 
